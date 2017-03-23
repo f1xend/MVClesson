abe2b5e7a7d3e20668234adf1c1678a85f6807ec
Ruslan <ruslan.muleev1785@gmail.com>
2017-03-23 20:34:53 +0400

    begin

---
+++ WebApplication5/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Доброе утро" : "Доброго дня";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guest)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guest);
+            }
+            else
+                return View();
+        }
+
+        
+        
+    }
+}
---
+++ WebApplication5/Models/GuestResponse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication5.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage ="Пожалуйста, Введите свое имя")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage ="Пожалуйста, введите email")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Вы ввели некоректный email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage ="Пожалуйста, введите телефон")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage ="Пожалуйста, укажите, примите ли Вы участие в вечеринке")]
+        public bool? WillAttend { get; set; }
+         
+    }
+}
