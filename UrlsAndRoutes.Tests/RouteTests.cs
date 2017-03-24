c035dd0a4e86c05ceb25bfca16421ec662c6bf72
Ruslan <sor1997@yandex.ru>
2017-03-24 18:11:21 +0300

    begin

---
+++ UrlsAndRoutes.Tests/RouteTests.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using System.Reflection;
+using System.Web;
+using System.Web.Routing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UrlsAndRoutes.Tests
+{
+    public class RouteTests
+    {
+        private HttpContextBase CreateHttpContext(string targetUrl = null,
+                                                  string httpMethod = "GET") 
+        {
+            Mock<HttpRequestBase> mockRequest = new Mock<HttpRequestBase>();
+            mockRequest.Setup(m => m.AppRelativeCurrentExecutionFilePath)
+                .Returns(targetUrl);
+            mockRequest.Setup(m => m.HttpMethod).Returns(httpMethod);
+
+
+            Mock<HttpResponseBase> mockResponse = new Mock<HttpResponseBase>();
+            mockResponse.Setup(m => m.ApplyAppPathModifier(
+                It.IsAny<string>())).Returns<string>(s => s);
+
+            Mock<HttpContextBase> mockContext = new Mock<HttpContextBase>();
+            mockContext.Setup(m => m.Request).Returns(mockRequest.Object);
+            mockContext.Setup(m => m.Response).Returns(mockResponse.Object);
+
+            return mockContext.Object;
+        }
+
+        private void TestRouteMatch(string url, string controller, string action, object routeProperties = null, string httpMethod = "GET")
+        {
+            RouteCollection routes = new RouteCollection();
+            RouteConfig.RegisterRoutes(routes);
+
+            RouteData result = routes.GetRouteData(CreateHttpContext(url, httpMethod));
+
+
+            Assert.IsNotNull(result);
+            Assert.IsTrue(TestIncomingRouteResult(result, controller, action, routeProperties));
+        }
+
+        private bool TestIncomingRouteResult(RouteData routeResult, string controller, string action, object propertySet = null)
+        {
+            Func<object, object, bool> valCompare = (v1, v2) =>
+            {
+                return StringComparer.InvariantCultureIgnoreCase.Compare(v1, v2) == 0;
+            };
+
+            bool result = valCompare(routeResult.Values["controller"], controller)
+                && valCompare(routeResult.Values["action"], action);
+
+            if (propertySet != null)
+            {
+                PropertyInfo[] propInfo = propertySet.GetType().GetProperties();
+                foreach(PropertyInfo pi in propInfo)
+                {
+                    if (!(routeResult.Values.ContainsKey(pi.Name)
+                        && valCompare(routeResult.Values[pi.Name],
+                        pi.GetValue(propertySet, null))))
+                    {
+                        result = false;
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+
+        private void TestRouteFail(string url)
+        {
+            RouteCollection routes = new RouteCollection();
+            RouteConfig.RegisterRoutes(routes);
+
+            RouteData result = routes.GetRouteData(CreateHttpContext(url));
+
+            Assert.IsTrue(result == null || result.Route == null);
+        }
+
+
+        [TestMethod]
+        public void TestIncomingRoutes()
+        {
+            TestRouteMatch("~/", "Admin", "Index", new { id = "DefaultId"});
+
+            TestRouteMatch("~/Home", "Home", "Index", new { id = "DefaultId" });
+
+            TestRouteMatch("~/Home/Index", "Home", "Index", new { id = "DefaultId" });
+
+            TestRouteFail("~/Home/Index/All/Delete");
+
+            TestRouteMatch("~/Home/Index/All", "Home", "Index", new { id = "All" });
+        }
+    }
+}
